4e84fcd52f66f88040816ff74ff0c77af66139bb
Pascal Schmid <psschmid@gmx.de>
2024-05-10 21:41:50 +0200

    Initial commit

---
+++ lib/find_old_mp3s.ex
@@ -0,0 +1,18 @@
+defmodule FindOldMp3s do
+  @moduledoc """
+  Documentation for `FindOldMp3s`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FindOldMp3s.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/find_old_mp3s_test.exs
@@ -0,0 +1,8 @@
+defmodule FindOldMp3sTest do
+  use ExUnit.Case
+  doctest FindOldMp3s
+
+  test "greets the world" do
+    assert FindOldMp3s.hello() == :world
+  end
+end
